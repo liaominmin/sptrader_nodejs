4972e3ff59e026a7ab12edfda63c682b619fa8c1
wanjochan <wanjochan@gmail.com>
2017-03-27 22:03:13 +0800

    better build.sh

--- rebuild.sh
+++ rebuild.sh
@@ -13,3 +13,7 @@ sh -c "node-gyp build 2>&1" > build/log_build.log
 cat build/log_build.log |grep -2 error
 
 ls -al build/Release/SpTrader.node
+NODE_MODULE_VER=`node -pe "var os=require('os');os.arch()+'-'+os.platform()+'-'+process.versions.modules"`
+BUILD_AND_RENAME=SpTrader.$NODE_MODULE_VER.node
+cp build/Release/SpTrader.node $BUILD_AND_RENAME
+ls -al $BUILD_AND_RENAME

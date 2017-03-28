c378f270f5e9212dd902fe23bdaa1c01291e64c7
wanjochan <wanjochan@gmail.com>
2017-03-28 18:20:21 +0800

    WJC: add docker supported

--- test_sptrader.js
+++ test_sptrader.js
@@ -1,5 +1,7 @@
-//NOTES
+//e.g.:
 //LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node test_sptrader.js
+//or
+//docker run -v"$PWD:/sptrader" -ti -w/sptrader cmptech/auto_ubuntu1610_nodejs_sharessl sh -c "LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so /node-v7.7.4/bin/node test_sptrader.js
 
 var logger=console;
 

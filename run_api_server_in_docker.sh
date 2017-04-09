c5dd0195ffb5c9b06bf36fea7ff99ae30739efb6
wanjochan <wanjochan@gmail.com>
2017-04-09 11:04:36 +0800

    run_api_server_in_docker.sh

--- run_api_server_in_docker.sh
+++ run_api_server_in_docker.sh
@@ -1,5 +1,6 @@
 export TZ=Asia/Hong_Kong
 echo run_api_server_in_docer.sh.date=`date`
-. /node_env.sh \
-	&& export PATH=/$NODE_VERSION/bin:$PATH \
-	&& LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js $*
+
+echo ". /node_env.sh && export PATH=/\$NODE_VERSION/bin:\$PATH && LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js \$*"
+
+. /node_env.sh && export PATH=/$NODE_VERSION/bin:$PATH && LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js $*

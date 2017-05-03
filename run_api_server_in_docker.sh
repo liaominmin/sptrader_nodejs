f5beb244061c56a247123a7d29b63954273abd4c
wanjochan <wanjochan@gmail.com>
2017-05-03 16:08:41 +0800

    WJC: thread pool size

--- run_api_server_in_docker.sh
+++ run_api_server_in_docker.sh
@@ -4,4 +4,5 @@ echo run_api_server_in_docer.sh.date=`date`
 echo ". /node_env.sh && export PATH=/\$NODE_VERSION/bin:\$PATH && LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js \$*"
 
 #. /node_env.sh && export PATH=/$NODE_VERSION/bin:$PATH && LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so /usr/bin/catchsegv node sptrader_api_server.js $*
-. /node_env.sh && export PATH=/$NODE_VERSION/bin:$PATH && UV_THREADPOOL_SIZE=120 LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js $*
+#. /node_env.sh && export PATH=/$NODE_VERSION/bin:$PATH && UV_THREADPOOL_SIZE=120 LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js $*
+. /node_env.sh && export PATH=/$NODE_VERSION/bin:$PATH && LD_LIBRARY_PATH=./ LD_PRELOAD=./libapiwrapper.so node sptrader_api_server.js $*
--- sptrader_api_server.js
+++ sptrader_api_server.js
@@ -1,8 +1,12 @@
+process.env.UV_THREADPOOL_SIZE = 126;
+
 //e.g.:
 //sh run_api_server_with_docker.sh -p=1234
 //TEST curl http://127.0.0.1:1234/ -d {m:Ping}
 
 var logger=console;
+
+logger.log(process.env);
 
 logger.log("__dirname=" + __dirname);
 
@@ -17,9 +21,13 @@ function argv2o(argv){
 var argo=argv2o(process.argv);
 //logger.log("sptrader_api_server.argo=",argo);
 
-var hhh,ppp;
+//NOTES: server_host/h && server_port/p for web,
+//host/port for sptrader...
+var hhh=argo.server_host||argo.h||'0.0.0.0',ppp=argo.server_port||argo.p||(()=>{
+	throw new Error("-server_port is mandatory")
+})();
 var logicModule=require(argo.logic||"./sptrader_api_server_demo_logic.js");
 var http_server=require('http').createServer(logicModule({argo}))
-	.listen(ppp=argo.server_port||argo.p||4321,hhh=argo.server_host||argo.h||'0.0.0.0',()=>{
-		logger.log(hhh+':'+ppp);
-	});
+	.listen(ppp,hhh,()=>{logger.log(hhh+':'+ppp)});
+
+//TODO if argo.ws_port

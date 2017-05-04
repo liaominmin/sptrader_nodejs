9ccb77bdeaf089ad9f5091d78078ad4167f41f85
wanjochan <wanjochan@gmail.com>
2017-05-04 23:29:39 +0800

    WJC: framework for ws

--- sptrader_api_server.js
+++ sptrader_api_server.js
@@ -30,4 +30,70 @@ var logicModule=require(argo.logic||"./sptrader_api_server_demo_logic.js");
 var http_server=require('http').createServer(logicModule({argo}))
 	.listen(ppp,hhh,()=>{logger.log(hhh+':'+ppp)});
 
-//TODO if argo.ws_port
+var ws_port=argo.ws_port,ws_host=argo.ws_host||'0.0.0.0';
+if(ws_port){
+	//TODO fwd logic to http server above in future
+	var _d_=logger.log;
+	var _client_conn_a={};//buffer of conn
+	try{
+		var ws = require("nodejs-websocket");
+		if(!ws){
+			_d_("nodejs-websocket module needed");
+			process.exit(2);
+		}
+		if(ws_port>1024){
+		}else{
+			_d_("port incorrect:"+ws_port);
+			process.exit(3);//NOTES: outside sh caller will not handle for case 3
+		}
+		_d_("pid=",process.pid);
+		process.on("exit",function(){
+			process.nextTick(function(){
+				_d_('This should not run');
+			});
+			_d_('About to exit.');
+		});
+		ws.setMaxBufferLength(20971520);
+		var ws_server = ws.createServer(
+			//{"secure":true},
+		);
+		ws_server.on('connection',function(conn){
+			var _addr=(conn.socket.remoteAddress);
+			var _port=(conn.socket.remotePort);
+			var _key=""+_addr+":"+_port;
+			_d_("on conn "+_key);
+			conn.key=_key;//用IP加PORT的方法来识别每个conn
+			_client_conn_a[_key]=conn;
+			conn.on("error", function (e){
+				_d_("ws_server.conn.error",e);
+			});
+			conn.on("text", function (data_s) {
+				_d_("on text",data_s);
+			});
+			conn.on("close", function (code, reason){
+				//clean up
+				_client_conn_a[_key]=null;
+				delete _client_conn_a[_key];
+				_d_("ws_server.close="+code+","+reason,"key="+this.key);
+			});
+		});
+		ws_server.on('error', function(e){
+			_d_("ws_server.error",e);
+			if (e.code == 'EADDRINUSE'){
+				_d_('Address in use');
+				process.exit(3);
+			}
+		});
+		_d_(" listen on "+ws_port);
+		ws_server.listen(ws_port);
+	}catch(ex){
+		_d_("ws.ex=",ex);
+	}
+}
+//process.on('SIGINT', function() {
+//   //db.stop(function(err) {
+//   //  process.exit(err ? 1 : 0);
+//   //});
+//	process.exit(0);
+//});
+
